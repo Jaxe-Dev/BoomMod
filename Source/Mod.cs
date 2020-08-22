186fb8a8df95f5faa70f93f09717d46f96217b9f
Jaxe <42095078+Jaxe-Dev@users.noreply.github.com>
2020-08-22 13:19:40 +0800

    v1.3
    - Added support for RimWorld 1.2
    - Removed support for RimWorld 1.0

--- Source/Mod.cs
+++ Source/Mod.cs
@@ -8,7 +8,7 @@ namespace BoomMod
     {
         public const string Id = "BoomMod";
         public const string Name = Id;
-        public const string Version = "1.2";
+        public const string Version = "1.3";
 
         static Mod() => new Harmony(Id).PatchAll();
     }
